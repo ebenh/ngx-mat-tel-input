57e9938bb621f6d82d183214f966352aae81f7ca
Ebenezer Hailemariam <eben@derso.org>
2021-08-16 14:16:44 -0400

    Test application now loads component from library

--- projects/ngx-mat-tel-input-test/src/app/app.module.ts
+++ projects/ngx-mat-tel-input-test/src/app/app.module.ts
@@ -3,12 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import {NgxMatTelInputModule} from 'ngx-mat-tel-input';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxMatTelInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
