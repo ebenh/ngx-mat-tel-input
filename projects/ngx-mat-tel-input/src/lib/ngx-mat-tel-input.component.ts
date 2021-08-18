4c4d269a4bb6f5e96be0fd81e8e1bd086958e27b
Ebenezer Hailemariam <eben@derso.org>
2021-08-18 19:35:21 -0400

    Stripped dead code

--- projects/ngx-mat-tel-input/src/lib/ngx-mat-tel-input.component.ts
+++ projects/ngx-mat-tel-input/src/lib/ngx-mat-tel-input.component.ts
@@ -1,18 +1,3 @@
-// @Component({
-//   selector: 'lib-ngx-mat-tel-input',
-//   templateUrl: './ngx-mat-tel-input.component.html',
-//   styleUrls: ['./ngx-mat-tel-input.component.scss']
-// })
-// export class NgxMatTelInputComponent implements OnInit {
-//
-//   constructor() {
-//   }
-//
-//   ngOnInit(): void {
-//   }
-//
-// }
-
 import {
   Component,
   OnInit,
