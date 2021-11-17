c82c0d87f15ce529eb90b3533cd8be02cc25204a
Ebenezer Hailemariam <eben@derso.org>
2021-11-17 16:59:48 -0500

    Added Home and End to allowed keys

--- projects/ngx-mat-tel-input/src/lib/ngx-mat-tel-input.component.ts
+++ projects/ngx-mat-tel-input/src/lib/ngx-mat-tel-input.component.ts
@@ -255,7 +255,7 @@ export class NgxMatTelInputComponent implements OnInit,
   onKeyDown(event: any): void {
     const validKeys = [
       '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '+',
-      'Backspace', 'Delete', 'Tab', 'ArrowLeft', 'ArrowRight'
+      'Backspace', 'Delete', 'Tab', 'ArrowLeft', 'ArrowRight', 'Home', 'End'
     ];
     if (validKeys.includes(event.key)) {
       return;
